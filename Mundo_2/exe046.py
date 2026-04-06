efde5268d4e61f133fde4a9f2334cd5466f7626e
Andreia Baptista <andreiabaptista4.4@gmail.com>
2026-04-06 16:04:51 +0100

    Exe 46: Implementação de loop for com time.sleep e playsound
    Uso do laço for com range decrescente.
    Importação de time.sleep para intervalos de 1 segundo.
    Integração da biblioteca playsound para reprodução de áudio MP3.

---
+++ Mundo_2/exe046.py
@@ -0,0 +1,9 @@
+from time import sleep
+from playsound import playsound
+for c in range (10, -1, -1):
+    print(c)
+    sleep(1)
+print ('🎆🎆 Feliz Ano  Novo 🎆🎆')
+sleep(0.5)
+print ('✨ BOOM!!BOOM!!POWWWW! ✨')
+playsound('fogos.mp3')
